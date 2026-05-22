46a2b8f340797403ed44e9e51ec9e84bc76bb663
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-22 14:25:07 -0600

    Update LiveMapComponent: modify selector and update default mapStyle URL

--- projects/tracking-map/src/lib/components/live-map-component/live-map-component.ts
+++ projects/tracking-map/src/lib/components/live-map-component/live-map-component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { MapComponent } from '@maplibre/ngx-maplibre-gl';
 
 @Component({
-  selector: 'lib-live-map-component',
+  selector: 'live-map-component',
   standalone: true,
   imports: [MapComponent],
   templateUrl: './live-map-component.html',
   styleUrl: './live-map-component.css',
 })
 export class LiveMapComponent {
-  mapStyle = 'https://demotiles.maplibre.org/style.json';
+  mapStyle = 'https://api.maptiler.com/maps/hybrid-v4/style.json?key=NOlJEA2Zes5006iTaZav';
 
   center: [number, number] = [-99.1332, 19.4326];
 
