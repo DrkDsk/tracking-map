c38b487fd0479eba8ac56d64f0197eecfd14c4ea
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-22 11:34:36 -0600

    Integrate MapLibre map into LiveMapComponent with basic configuration.

--- projects/tracking-map/src/lib/components/live-map-component/live-map-component.ts
+++ projects/tracking-map/src/lib/components/live-map-component/live-map-component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MapComponent } from '@maplibre/ngx-maplibre-gl';
 
 @Component({
   selector: 'lib-live-map-component',
-  imports: [],
+  standalone: true,
+  imports: [MapComponent],
   templateUrl: './live-map-component.html',
   styleUrl: './live-map-component.css',
 })
+export class LiveMapComponent {
+  mapStyle = 'https://demotiles.maplibre.org/style.json';
 
-export class LiveMapComponent {}
+  center: [number, number] = [-99.1332, 19.4326];
+
+  zoom: [number] = [12];
+}
