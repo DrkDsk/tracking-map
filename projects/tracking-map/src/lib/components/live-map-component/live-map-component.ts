b1984de31692a82d40b3ba92ea1de58943d8caf3
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-22 17:35:33 -0600

    Add route rendering logic to LiveMapComponent with GeoJSON source and RoutingService integration

--- projects/tracking-map/src/lib/components/live-map-component/live-map-component.ts
+++ projects/tracking-map/src/lib/components/live-map-component/live-map-component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { MapComponent } from '@maplibre/ngx-maplibre-gl';
+import { GeoJSONSource, Map } from 'maplibre-gl';
+import { inject } from '@angular/core';
+import { RoutingService } from '../../services/maps/routing_service';
+import { RouteConfig } from '../../core/models/route_config';
 
 @Component({
   selector: 'live-map-component',
@@ -9,9 +13,56 @@ import { MapComponent } from '@maplibre/ngx-maplibre-gl';
   styleUrl: './live-map-component.css',
 })
 export class LiveMapComponent {
+  private routingService = inject(RoutingService);
+
   mapStyle = 'https://api.maptiler.com/maps/hybrid-v4/style.json?key=NOlJEA2Zes5006iTaZav';
+  zoom: [number] = [6];
 
-  center: [number, number] = [-99.1332, 19.4326];
+  private map!: Map;
 
-  zoom: [number] = [12];
+  onMapLoad(map: Map) {
+    this.map = map;
+
+    this.map.addSource('route', {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        geometry: {
+          type: 'LineString',
+          coordinates: [],
+        },
+        properties: {},
+      },
+    });
+
+    this.map.addLayer({
+      id: 'route-line',
+      type: 'line',
+      source: 'route',
+      paint: {
+        'line-color': '#d93341',
+        'line-width': 6,
+      },
+    });
+
+    this.routingService.getRoute([-93.74744249, 16.0853598], [-93.06810014, 16.74755547])
+      .subscribe((route) => {
+        const coordinates = route.routes[0].geometry.coordinates;
+        this.drawRoute(coordinates);
+      });
+  }
+
+  drawRoute(coordinates: number[][]) {
+    if (this.map.getSource('route')) {
+      (this.map.getSource('route') as GeoJSONSource).setData({
+        type: 'Feature',
+        geometry: {
+          type: 'LineString',
+          coordinates,
+        },
+        properties: {},
+      });
+      return;
+    }
+  }
 }
