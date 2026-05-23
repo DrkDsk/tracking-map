75da9f8d2b578e5269ee9bd32bca8dd792aa9330
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-22 19:03:43 -0600

    Update LiveMapComponent: add @Input() for mapStyle, zoom, and mexicoBounds to enhance configurability

--- projects/tracking-map/src/lib/components/live-map-component/live-map-component.ts
+++ projects/tracking-map/src/lib/components/live-map-component/live-map-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MapComponent } from '@maplibre/ngx-maplibre-gl';
 import { LngLatLike, Map } from 'maplibre-gl';
 import { inject } from '@angular/core';
@@ -16,13 +16,19 @@ export class LiveMapComponent {
   private routingService = inject(RoutingService);
   private mapService = inject(MapService);
 
+  @Input()
   mapStyle = 'https://api.maptiler.com/maps/hybrid-v4/style.json?key=NOlJEA2Zes5006iTaZav';
+
+  @Input()
   zoom: [number] = [1];
-  center: [number, number] = [-102.5528, 23.6345];
+
+  @Input()
   mexicoBounds: [LngLatLike, LngLatLike] = [
     [-130, 5],
     [-75, 40],
   ];
+
+  center: [number, number] = [-102.5528, 23.6345];
 
   onMapLoad(map: Map) {
     this.mapService.initialize(map);
