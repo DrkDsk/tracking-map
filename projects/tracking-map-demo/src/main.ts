2bdfcf4ea8766a485a19de8f1092934b1facb7f6
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-23 20:39:03 -0600

    Import MapLibre CSS in main.ts to enable map styling

--- projects/tracking-map-demo/src/main.ts
+++ projects/tracking-map-demo/src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
