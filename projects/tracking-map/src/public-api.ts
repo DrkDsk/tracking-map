6afb865a5393c11e10488358ba34018c23721aa2
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-25 13:41:18 -0600

    Export core enums, models, and real-time tracking services in public API

--- projects/tracking-map/src/public-api.ts
+++ projects/tracking-map/src/public-api.ts
@@ -4,3 +4,8 @@
 
 export * from './lib/components/live-map-component/live-map-component';
 export * from './lib/components/reply-map-component/reply-map-component';
+export * from './lib/core/enums/provider_type';
+export * from './lib/core/models/tracking_position';
+export * from './lib/core/models/websocket_connection_state';
+export * from './lib/core/services/socket/realtime-tracking.service';
+export * from './lib/core/services/socket/unit-state.service';
