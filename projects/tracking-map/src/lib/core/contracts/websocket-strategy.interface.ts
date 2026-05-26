2c48a23620741e443b97e036c494d2ba10bc16fd
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-25 18:54:07 -0600

    Refactor `WebsocketMessage` and `WebsocketStrategy` interfaces: add generic payload support and replace `unit

--- projects/tracking-map/src/lib/core/contracts/websocket-strategy.interface.ts
+++ projects/tracking-map/src/lib/core/contracts/websocket-strategy.interface.ts
@@ -1,10 +1,11 @@
 import { ClientType } from '../enums/provider_type';
 import { TrackingPosition } from '../models/tracking_position';
+import { TrackingUnitInput } from '../models/tracking_unit_reference';
 import { WebsocketConfig } from '../models/websocket_config';
 
 export interface WebsocketStrategy {
   readonly provider: ClientType;
-  getConfig(unitId: string | number): WebsocketConfig;
-  parse(payload: unknown, unitId: string | number): TrackingPosition | null;
-  shouldHandle(position: TrackingPosition, unitId: string | number): boolean;
+  getConfig(unit: TrackingUnitInput): WebsocketConfig;
+  parse(payload: unknown, unit: TrackingUnitInput): TrackingPosition | null;
+  shouldHandle(position: TrackingPosition, unit: TrackingUnitInput): boolean;
 }
--- projects/tracking-map/src/lib/core/models/websocket_message.ts
+++ projects/tracking-map/src/lib/core/models/websocket_message.ts
@@ -1,6 +1,6 @@
 import { WebsocketChannelSubscription } from './websocket_channel_subscription';
 
-export interface WebsocketMessage extends WebsocketChannelSubscription {
-  payload: unknown;
+export interface WebsocketMessage<TPayload = unknown> extends WebsocketChannelSubscription {
+  payload: TPayload;
   received_at: number;
 }
