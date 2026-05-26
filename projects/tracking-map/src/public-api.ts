95bc06af65d40131c1b77c8e1108eb934520bef2
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-25 18:42:50 -0600

    Export `TrackingUnitReference` model in public API for broader usage in the library.

--- projects/tracking-map/src/public-api.ts
+++ projects/tracking-map/src/public-api.ts
@@ -6,6 +6,7 @@ export * from './lib/components/live-map-component/live-map-component';
 export * from './lib/components/reply-map-component/reply-map-component';
 export * from './lib/core/enums/provider_type';
 export * from './lib/core/models/tracking_position';
+export * from './lib/core/models/tracking_unit_reference';
 export * from './lib/core/models/websocket_connection_state';
 export * from './lib/core/services/socket/realtime-tracking.service';
 export * from './lib/core/services/socket/unit-state.service';
