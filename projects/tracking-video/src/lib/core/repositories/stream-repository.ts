2a0d8bca9b78d0da065e68d52d961b4051bc49a8
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-27 15:59:21 -0600

    feat: add `Observable` return type to `getStreamUrls` & update demo to use `tracking-video-component` exclusively

--- projects/tracking-video/src/lib/core/repositories/stream-repository.ts
+++ projects/tracking-video/src/lib/core/repositories/stream-repository.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiProviderResolver } from '../strategy/api.provider.resolver';
 import { ClientType } from 'tracking-map';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ import { ClientType } from 'tracking-map';
 export class StreamRepository {
   private resolver = inject(ApiProviderResolver);
 
-  getStreamUrls(provider: ClientType, unitId: string | number) {
+  getStreamUrls(provider: ClientType, unitId: string | number): Observable<string[]> {
     return this.resolver.resolve(provider).getStreamUrls(unitId);
   }
 }
