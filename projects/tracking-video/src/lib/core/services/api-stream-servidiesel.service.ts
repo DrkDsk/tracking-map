f64f2c4034269f43d609c44658d01acd2602878f
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-27 11:02:58 -0600

    feat: update `ApiProviderResolver` to handle multiple stream services and stub `getStreamUrls` in `ApiStreamServiDieselService`

--- projects/tracking-video/src/lib/core/services/api-stream-servidiesel.service.ts
+++ projects/tracking-video/src/lib/core/services/api-stream-servidiesel.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiStreamService } from '../contracts/api_stream.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { StreamVideoResponse } from '../models/stream_video_response';
 import { ApiClient } from '../http/api-client.service';
 import { environment } from '../../../environments/environment';
@@ -14,6 +14,13 @@ export class ApiStreamServiDieselService implements ApiStreamService {
   private readonly baseUrl = environment.providers.servidiesel;
 
   getStreamUrls(unitId: string | number): Observable<StreamVideoResponse> {
-    return this.api.get<StreamVideoResponse>(this.baseUrl, `/api/units/${unitId}}/stream-video`);
+    return of<StreamVideoResponse>({
+      urls: [
+        'https://america-ftcloud.ifleetvision.com:22001/video/21389026-7eb9-5327-590-b5bf690e3c737.flv',
+        'https://america-ftcloud.ifleetvision.com:22001/video/e9fa90c2-fba9-f50a-090-ce90c905216c9.flv',
+      ],
+    });
+
+    //return this.api.get<StreamVideoResponse>(this.baseUrl, `/api/units/${unitId}}/stream-video`);
   }
 }
