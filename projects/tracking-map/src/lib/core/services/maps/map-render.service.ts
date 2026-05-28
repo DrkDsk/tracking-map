7acc8461ee6df7de2bbd0c8e3a101647d4be55a1
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-28 10:07:39 -0600

    feat: comment out provider field in map popups in `map-render.service`

--- projects/tracking-map/src/lib/core/services/maps/map-render.service.ts
+++ projects/tracking-map/src/lib/core/services/maps/map-render.service.ts
@@ -445,6 +445,7 @@ export class MapRenderService {
   <div style="color:#000000; font-family: system-ui; font-size: 12px;">
     <strong style="color:#000000;">${properties['label']}</strong>
 
+    <!-- <div>Proveedor: ${properties['provider']}</div> -->
     <div>Latitud: ${properties['lat']}</div>
     <div>Longitude: ${properties['lng']}</div>
     <div>Velocidad: ${properties['speed']} km/h</div>
